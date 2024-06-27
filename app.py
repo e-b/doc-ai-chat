c41c691280ca505e547e22d39f21bdee369ac6a9
Eric Ballnath <ballnath@gmail.com>
2024-06-27 15:33:01 +0200

    modified:   app.py deleted:    app2.py

--- app.py
+++ app.py
@@ -1,93 +1,104 @@
-import streamlit as st
+import gradio as gr
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
+
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+#from langchain_google_vertexai import VertexAIEmbeddings
+#from google.cloud import aiplatform
+
 import google.generativeai as genai
 
-# from langchain.vectorstores import Chroma
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
+
+""" PROJECT="genai-lab-390210"
+LOCATION="europe-west3"
+aiplatform.init(project=PROJECT, location=LOCATION) """
+
+
 load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
-def get_pdf_text(pdf_docs):
+model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.0)
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="models/multilingual-embedding-002"
+)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+
+
+def embed_files(file_paths):
+    paths = [file.name for file in file_paths]
+    for path in paths:
+        text = text_from_pdf(path)
+        chunks = chunks_from_text(text)
+        faiss_embed(chunks)
+
+
+def text_from_pdf(pdf_path):
+    loader = PyPDFLoader(pdf_path)
+    docs = loader.load()
     text = ""
-    for pdf in pdf_docs:
-        pdf_loader = PyPDFLoader(pdf)
-        for page in pdf_loader.pages:
-            text += page.extract_text()
+    for doc in docs:
+        print(doc.page_content)
+        text += doc.page_content
     return text
 
 
-def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+def chunks_from_text(text):
     chunks = text_splitter.split_text(text)
     return chunks
 
 
-def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding--001")
+def faiss_embed(text_chunks):
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
 
 
 def get_conversational_chain():
-    prompt_template = """
+    prompt = """
     Beantworte die Frage so detailliert wie möglich aus dem Kontext.
-    Wenn die Frage sich nicht aus dem Kontext beantworten lässt, antworte "Die Frage kann nicht aus dem Kontext beantwortet werden".
+    Wenn die Frage sich nicht aus dem Kontext beantworten lässt, antworte mit "Die Frage kann nicht aus dem Kontext beantwortet werden".
     Kontext:\n{context}\n
     Frage:\n{question}\n
 
     Antwort:
     """
 
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.0)
-
-    prompt = PromptTemplate(
-        template=prompt_template, input_variables=["context", "question"]
-    )
+    prompt = PromptTemplate(template=prompt, input_variables=["context", "question"])
 
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
     return chain
 
 
-def user_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-    new_db = FAISS.load_local("faiss-index", embeddings=embeddings)
-    docs = new_db.similarity_search(user_question)
+def ask(user_question):
+    db = FAISS.load_local("faiss-index", embeddings=embeddings)
+    docs = db.similarity_search(user_question)
     chain = get_conversational_chain()
     response = chain(
         {"input_documents": docs, "question": user_question}, return_only_outputs=True
     )
     print(response)
-    st.write("Antwort: ", response["output_text"])
 
 
 def main():
-    st.set_page_config("Chat mit Dokumenten")
-    st.header("Gemini-powered Chat mit Dokumenten")
-
-    user_question = st.text_input("Stellen Sie eine Frage.")
-    if user_question:
-        user_input(user_question)
-
-    with st.sidebar:
-        st.title("Menü:")
-        pdf_docs = st.file_uploader(
-            "Wählen Sie die PDF Dokumente aus und klicken Sie auf 'laden'"
+    with gr.Blocks() as demo:
+        file_output = gr.File()
+        upload_button = gr.UploadButton(
+            "click to upload a file",
+            file_count="multiple",
         )
-        if st.button("laden"):
-            with st.spinner("verarbeite ..."):
-                raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_text_chunks(raw_text)
-                get_vector_store(text_chunks)
-                st.success("Done")
+        upload_button.upload(fn=embed_files, inputs=upload_button, outputs=file_output)
+        question = gr.TextArea(label="question", lines=8)
+        submit = gr.Button("submit")
+        answer = gr.TextArea(label="answer", lines=8)
+        submit.click(fn=ask, inputs=question, outputs=answer)
+    demo.launch()
 
 
 if __name__ == "__main__":
